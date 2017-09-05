606822ccedda3c8b9eabfb2ea5a3563214aebc9b
snow-orld <mou.xueman@gmail.com>
2017-09-05 23:15:37 +0800

    card constructor update

--- classes.py
+++ classes.py
@@ -4,7 +4,7 @@
 @file    classes.py
 @author  Cecilia M.
 @date    2017-09-01
-@version $Id: classes.py 01 2017-09-01 22:02: behrisch $
+@version $Id: classes.py 02 2017-09-05 23:01: behrisch $
 
 This script descrbes the basic component that appears 
 in the SIF game.
@@ -15,13 +15,15 @@ from random import random
 
 class Card(object):
 	""" Base Class for all cards in the game"""
-	def __init__(self, cid, name, attribute, rank, maxpoint=0, skill='None', effect='', leaderskill='None', leadereffect=''):
+	def __init__(self, cid, name, attribute, rank, maxsmile=0, maxpure=0, maxcool=0, skill='None', effect='', leaderskill='None', leadereffect='', version='', releasedate=''):
 		super(Card, self).__init__()
 		self.id = int(cid)	# id of card across the game
 		self.name = name	# member name of the card
 		self.attribute = attribute	# smile, pure, or cool
 		self.rank = rank	# N, R, SR, SSR, UR
-		self.maxpoint = int(maxpoint)	# max attribute point when reaching max level when idolized
+		self.maxsmile = int(maxsmile)	# attribute point when reaching max level when idolized
+		self.maxpure = int(maxpure)
+		self.maxcool = int(maxcool)
 
 		# skill fields are None and '' for N cards
 		self.skill = skill	# skill name of the card
@@ -62,20 +64,17 @@ class Card(object):
 	def practice(self, exprience):
 		pass
 
-	def addskill(self, skill):
-		pass
-
 class NCard(Card):
 	"""Class for N Card"""
-	def __init__(self, cid, name, attribute, maxpoint):
-		super(NCard, self).__init__(cid, name, attribute, 'N', maxpoint)
+	def __init__(self, cid, name, attribute, maxsmile, maxpure, maxcool):
+		super(NCard, self).__init__(cid, name, attribute, 'N', maxsmile, maxpure, maxcool)
 		self.maxlevel = 30
 		self.maxbond = 25
 		
 class RCard(Card):
 	"""Class for R Card"""
-	def __init__(self, cid, name, attribute, maxpoint, skill, effect, leaderskill, leadereffect):
-		super(RCard, self).__init__(cid, name, attribute, 'R', maxpoint, skill, effect, leaderskill, leadereffect)
+	def __init__(self, cid, name, attribute, maxsmile, maxpure, maxcool, skill, effect, leaderskill, leadereffect, version, releasedate):
+		super(RCard, self).__init__(cid, name, attribute, 'R', maxsmile, maxpure, maxcool, skill, effect, leaderskill, leadereffect, version, releasedate)
 		self.maxlevel = 40
 		self.maxbond = 100
 		self.skillslot = 1
@@ -83,8 +82,8 @@ class RCard(Card):
 
 class SRCard(Card):
 	"""Class for SR Card"""
-	def __init__(self, cid, name, attribute, maxpoint, skill, effect, leaderskill, leadereffect):
-		super(SRCard, self).__init__(cid, name, attribute, 'SR', maxpoint, skill, effect, leaderskill, leadereffect)
+	def __init__(self, cid, name, attribute, maxsmile, maxpure, maxcool, skill, effect, leaderskill, leadereffect, version, releasedate):
+		super(SRCard, self).__init__(cid, name, attribute, 'SR', maxsmile, maxpure, maxcool, skill, effect, leaderskill, leadereffect, version, releasedate)
 		self.maxlevel = 60
 		self.maxbond = 250
 		self.skillslot = 2
@@ -92,8 +91,8 @@ class SRCard(Card):
 		
 class SSRCard(Card):
 	"""Class for SSR Card"""
-	def __init__(self, cid, name, attribute, maxpoint, skill, effect, leaderskill, leadereffect):
-		super(SSRCard, self).__init__(cid, name, attribute, 'SSR', maxpoint, skill, effect, leaderskill, leadereffect)
+	def __init__(self, cid, name, attribute, maxsmile, maxpure, maxcool, skill, effect, leaderskill, leadereffect, version, releasedate):
+		super(SSRCard, self).__init__(cid, name, attribute, 'SSR', maxsmile, maxpure, maxcool, skill, effect, leaderskill, leadereffect, version, releasedate)
 		self.maxlevel = 70
 		self.maxbond = 375
 		self.skillslot = 3
@@ -101,8 +100,8 @@ class SSRCard(Card):
 		
 class URCard(Card):
 	"""Class for UR Card"""
-	def __init__(self, cid, name, attribute, maxpoint, skill, effect, leaderskill, leadereffect):
-		super(URCard, self).__init__(cid, name, attribute, 'UR', maxpoint, skill, effect, leaderskill, leadereffect)
+	def __init__(self, cid, name, attribute, maxsmile, maxpure, maxcool, skill, effect, leaderskill, leadereffect, version, releasedate):
+		super(URCard, self).__init__(cid, name, attribute, 'UR', maxsmile, maxpure, maxcool, skill, effect, leaderskill, leadereffect, version, releasedate)
 		self.maxlevel = 80
 		self.maxbond = 500
 		self.skillslot = 4
@@ -110,8 +109,8 @@ class URCard(Card):
 
 class URGiftCard(URCard):
 	"""Class for UR Card given as a gift"""
-	def __init__(self, cid, name, attribute, maxpoint, skill, effect, leaderskill, leadereffect):
-		super(URGiftCard, self).__init__(cid, name, attribute, maxpoint, skill, effect, leaderskill, leadereffect)
+	def __init__(self, cid, name, attribute, maxsmile, maxpure, maxcool, skill, effect, leaderskill, leadereffect, version, releasedate):
+		super(URGiftCard, self).__init__(cid, name, attribute, maxsmile, maxpure, maxcool, skill, effect, leaderskill, leadereffect, version, releasedate)
 		self.idolize()
 		self.skillslot = 2
 		self.maxskillslot = 2
@@ -123,15 +122,15 @@ if __name__ == '__main__':
 		name = US[int(random() * len(US))]
 		attribute = CARDATTRIBUTES[int(random() * len(CARDATTRIBUTES))]
 		if rank == 'N':
-			card = NCard(cid, name, attribute, 0)
+			card = NCard(cid, name, attribute, 0, 0, 0)
 		if rank == 'R':
-			card = RCard(cid, name, attribute, 0, '', '', '', '')
+			card = RCard(cid, name, attribute, 0, 0, 0, '', '', '', '', '', '')
 		if rank == 'SR':
-			card = SRCard(cid, name, attribute, 0, '', '', '', '')
+			card = SRCard(cid, name, attribute, 0, 0, 0, '', '', '', '', '', '')
 		if rank == 'SSR':
-			card = SSRCard(cid, name, attribute, 0, '', '', '', '')
+			card = SSRCard(cid, name, attribute, 0, 0, 0, '', '', '', '', '', '')
 		if rank == 'UR':
-			card = URCard(cid, name, attribute, 0, '', '', '', '')
+			card = URCard(cid, name, attribute, 0, 0, 0, '', '', '', '', '', '')
 		if rank == 'UR-GIFT':
-			card = URGiftCard(cid, name, attribute, 0, '', '', '', '')
-		print('%s\t#%d%s%s\t%s\tmax %d\tlevel %d/%d\tbond %d/%d\tskillslot %d/%d' % (card.name, card.id, ' ' * (7-len(str(card.id))), card.rank, card.attribute, card.maxpoint, card.level, card.maxlevel, card.bond, card.maxbond, card.skillslot, card.maxskillslot))
+			card = URGiftCard(cid, name, attribute, 0, 0, 0, '', '', '', '', '', '')
+		print('%s\t#%d%s%s\t%s\tmax %d,%d,%d\tlevel %d/%d\tbond %d/%d\tskillslot %d/%d' % (card.name, card.id, ' ' * (7-len(str(card.id))), card.rank, card.attribute, card.maxsmile, card.maxpure, card.maxcool, card.level, card.maxlevel, card.bond, card.maxbond, card.skillslot, card.maxskillslot))
